35077f984908e7f55cca3e58e4b6b406178c813e
kkoon9 <rndrnjs2003@naver.com>
2019-02-20 22:14:24 +0900

    Add BOJ11727
    BOJ code and comment

---
+++ BOJ11727.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int memo[1001];
+
+int main() {
+	memo[0] = 1;
+	memo[1] = 1;
+	int n;
+	cin >> n;
+	for (int i = 2; i <= n; i++) {
+		memo[i] = memo[i - 1] + 2 * memo[i - 2];
+		memo[i] %= 10007;
+	}
+	cout << memo[n] << endl;
+	return 0;
+}
