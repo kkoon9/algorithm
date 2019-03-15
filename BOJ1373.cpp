a3f772f4a53ab002e8caf907c49745dd07b6e934
kkoon9 <rndrnjs2003@naver.com>
2019-03-16 08:45:34 +0900

    Add BOJ1373
    BOJ code and comment

---
+++ BOJ1373.cpp
@@ -0,0 +1,57 @@
+// 1번째 접근
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+long long square(int a) {
+	long long result = 1;
+	for (int i = 0; i < a; i++)
+		result *= 2;
+	return result;
+}
+int main() {
+	string s;
+	cin >> s;
+	int size = s.size();
+	stack<int> st;
+	int j = 0;
+	int output = 0;
+	for (int i = size - 1; i >= 0; i--) {
+		int x = s[i] - '0';
+		output += x * square(j++);
+		if (j == 3) {
+			st.push(output);
+			j = 0;
+			output = 0;
+		}
+	}
+	if(j!=0)
+		st.push(output);
+	while (!st.empty()) {
+		int x = st.top();
+		st.pop();
+		printf("%d", x);
+	}
+	cout << '\n';
+	return 0;
+}
+
+// 더 나은 코드
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s;
+    cin >> s;
+    int n = s.size();
+    if (n%3 == 1) 
+        cout << s[0];
+    else if (n%3 == 2)
+        cout << (s[0]-'0')*2 + (s[1]-'0');
+    
+    for (int i=n%3; i<n; i+=3)
+        cout << (s[i]-'0')*4 + (s[i+1]-'0')*2 + (s[i+2]-'0');
+    
+    cout << '\n';
+    return 0;
+}
