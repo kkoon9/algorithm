1c5b3bb4905733ec6894e833a4c0a6feaf476fc3
kkoon9 <rndrnjs2003@naver.com>
2019-03-17 20:33:11 +0900

    Add BOJ1676
    BOJ code and comment

---
+++ BOJ1676.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int memo[501]; // 소인수 분해 했을 때 소수들의 각 개수
+void PF(int n) {
+	for (int i = 2; i*i <= n; i++) {
+		while (n%i == 0) {
+			memo[i]++;
+			n /= i;
+		}
+	}
+	if (n > 1)
+		memo[n]++;
+}
+void Fac(int n) {
+	if (n == 0)
+		return;
+	PF(n);
+	Fac(n - 1);
+}
+int main() {
+	int n;
+	cin >> n;
+	Fac(n);
+	cout << memo[5] << '\n';
+	return 0;
+}
