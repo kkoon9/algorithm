c1c5dda4fea5e694a38acf16aeaebcfbbb212390
kkoon9 <rndrnjs2003@naver.com>
2019-03-23 15:46:56 +0900

    Modify BOJ2346
    코드 및 코멘트 수정

--- BOJ2346.cpp
+++ BOJ2346.cpp
@@ -3,53 +3,53 @@
 using namespace std;
 int main() {
 	int n;
-	list<pair<int, int>> a;
+	list<pair<int, int>> l;
 	cin >> n;
 
 	for (int i = 1; i <= n; i++) {
 		int x;
 		cin >> x;
-		a.push_back({ x,i });
+		l.push_back({ x,i });
 	}
 
-	auto it = a.begin();
-	for (int i = 0; i<n - 1; i++) {
-		cout << (it->second) << ' ';
+	auto it = l.begin();
+	for (int i = 0; i < n - 1; i++) {
+		cout << (it->second) << ' '; // 해당 step 원소 출력
 		int step = it->first;
 		if (step > 0) {
 			auto temp = it;
 			++temp;
-			if (temp == a.end()) {
-				temp = a.begin();
+			if (temp == l.end()) { // 해당 step 원소가 끝이었으면 시작으로 되돌려준다. 
+				temp = l.begin();
 			}
-			a.erase(it);
+			l.erase(it);
 			it = temp;
-			for (int i = 1; i<step; i++) {
+			for (int i = 1; i < step; i++) {
 				++it;
-				if (it == a.end()) {
-					it = a.begin();
+				if (it == l.end()) {
+					it = l.begin();
 				}
 			}
 		}
 		else if (step < 0) {
 			step = -step;
 			auto temp = it;
-			if (temp == a.begin()) {
-				temp = a.end();
+			if (temp == l.begin()) {
+				temp = l.end();
 			}
 			--temp;
-			a.erase(it);
+			l.erase(it);
 			it = temp;
-			for (int i = 1; i<step; i++) {
-				if (it == a.begin()) {
-					it = a.end();
+			for (int i = 1; i < step; i++) {
+				if (it == l.begin()) {
+					it = l.end();
 				}
 				--it;
 			}
 		}
 	}
 
-	cout << a.front().second << '\n';
+	cout << l.front().second << '\n';
 
 	return 0;
 }
