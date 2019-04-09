75fc77ced4f15b012d38d84b6d9aec3868390c50
kkoon9 <rndrnjs2003@naver.com>
2019-04-09 19:59:21 +0900

    Add BOJ1967
    BOJ code and comment

---
+++ BOJ1967.cpp
@@ -0,0 +1,38 @@
+#pragma warning(disable: 4996)
+#include <iostream>
+#include <vector>
+using namespace std;
+vector <pair<int, int>> dis[10001];
+bool check[10001];
+int RtoNode = 0; // root에서 가장 먼 정점
+
+long long diameter = 0; // 지름
+
+void dfs(int node, int dt) { // node : 정점, dt : distance, 거리
+	if (check[node]) return;
+	check[node] = true;
+	if (diameter < dt) {
+		diameter = dt;
+		RtoNode = node;
+	}
+	for (int i = 0; i < dis[node].size(); i++)
+		dfs(dis[node][i].first, dt + dis[node][i].second);
+}
+
+int main() {
+	int n; // 정점의 개수
+	int v_num, a, b;
+	cin >> n;
+	for (int i = 1; i < n; i++) {
+		scanf("%d %d %d", &v_num, &a, &b);
+		dis[v_num].push_back(make_pair(a, b));
+		dis[a].push_back(make_pair(v_num, b));
+	}
+	dfs(1, 0);
+	for (int i = 1; i <= n; i++)
+		check[i] = false;
+	diameter = 0;
+	dfs(RtoNode, 0);
+	cout << diameter << '\n';
+	return 0;
+}
