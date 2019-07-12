e1fe7be49bead519c1fde3741ec442919e61ca54
kkoon9 <rndrnjs2003@naver.com>
2019-07-12 13:44:37 +0900

    Add BOJ1660 캡틴 이다솜
    최소 사면체의 개수를 가지는 DP도 작성하여 문제를 해결.

--- BOJ1660.cpp
+++ BOJ1660.cpp
@@ -2,25 +2,33 @@
 #include <string>
 
 using namespace std;
-const int memoSize = 126;
+const int memoSize = 130;
+const int NSize = 300001;
 int memo[memoSize];
+int dp[NSize];
+
+void DP(int memo[], int N) {
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= memoSize; j++) {
+			if (memo[j] <= i) {
+				int tmp = dp[i - memo[j]];
+				if (tmp != NSize && tmp + 1 < dp[i])
+					dp[i] = tmp + 1;				
+			}
+		}
+	}
+}
 int main() {
 	int N; // input
-	int result = 0;
 	memo[1] = 1;
 	for (int i = 2; i < memoSize; i++) {
 		memo[i] = memo[i - 1] + (i)*(i + 1) / 2;
-		cout << memo[i] << '\n';
+		//		cout << memo[i] << '\n';
 	}
 	cin >> N;
-	for (int i = memoSize - 1; i > 0; i--) {
-		while (N >= memo[i]) {
-			N -= memo[i];
-			cout << memo[i] << '\n';
-			result++;
-		}
-		if (N == 0)break;
-	}
-	cout << result << '\n';
+	for (int i = 1; i <= N; i++)
+		dp[i] = NSize;	
+	DP(memo, N);
+	cout << dp[N] << '\n';
 	return 0;
 }
