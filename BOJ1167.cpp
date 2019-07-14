ae9d6f3b48f9e00e49749353dd771f4f26928054
kkoon9 <rndrnjs2003@naver.com>
2019-07-14 21:54:01 +0900

    refactoring BOJ1167 트리의 지름
    코드를 함수화하여 main문을 간소화시켰다.
    상수를 변수화시켜줬다.
    dfs를 사용한다.

--- BOJ1167.cpp
+++ BOJ1167.cpp
@@ -2,23 +2,16 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-vector <pair<int, int>> dis[100001];
-bool check[100001];
-int RtoNode = 0; // root에서 가장 먼 정점
 
+const int MAX = 100001;
+int RtoNode = 0; // root에서 가장 먼 정점
 long long diameter = 0; // 지름
 
-void dfs(int node, int dt) { // node : 정점, dt : distance, 거리
-	if (check[node]) return;
-	check[node] = true;
-	if (diameter < dt) {
-		diameter = dt;
-		RtoNode = node;
-	}
-	for (int i = 0; i < dis[node].size(); i++)
-		dfs(dis[node][i].first, dt + dis[node][i].second);
-}
+vector <pair<int, int>> dis[MAX];
+bool check[MAX];
 
+void dfs(int node, int dt);
+void init(bool check[], int V);
 int main() {
 	int V; // 정점의 개수
 	int v_num, a, b;
@@ -33,10 +26,25 @@ int main() {
 		}
 	}
 	dfs(1, 0);
-	for (int i = 1; i <= V; i++)
-		check[i] = false;
+	init(check, V);
 	diameter = 0;
 	dfs(RtoNode, 0);
 	cout << diameter << '\n';
 	return 0;
+}
+
+void init(bool check[], int V) { 
+	for (int i = 1; i <= V; i++)
+		check[i] = false;
+}
+
+void dfs(int node, int dt) { // node : 정점, dt : distance, 거리
+	if (check[node]) return;
+	check[node] = true;
+	if (diameter < dt) {
+		diameter = dt;
+		RtoNode = node;
+	}
+	for (int i = 0; i < dis[node].size(); i++)
+		dfs(dis[node][i].first, dt + dis[node][i].second);
 }
