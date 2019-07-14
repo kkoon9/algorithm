c7d152fa00b6e1d6ebfa1182e623da58a50c1987
kkoon9 <rndrnjs2003@naver.com>
2019-07-14 22:16:39 +0900

    refactoring BOJ1212 8진수 2진수
    코드를 함수화하여 main문을 간소화시켰다.

--- BOJ1212.cpp
+++ BOJ1212.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+void octNum(int x);
+void firstNum(int x);
+
 int main() {
 	string s;
 	cin >> s;
 	int n = s.size();
 	int x = s[0] - '0';
+	firstNum(x);
+	for (int i = 1; i < n; i++) {
+		x = s[i] - '0';
+		octNum(x);
+	}
+	return 0;
+}
+void firstNum(int x) {
 	switch (x) {
 	case 7:
 		cout << "111";
@@ -30,41 +42,38 @@ int main() {
 		break;
 	case 0:
 		cout << "0";
-		return 0;
+		break;
 	default:
 		break;
 	}
-	for (int i = 1; i < n; i++) {
-		x = s[i] - '0';
-		switch (x) {
-		case 7:
-			cout << "111";
-			break;
-		case 6:
-			cout << "110";
-			break;
-		case 5:
-			cout << "101";
-			break;
-		case 4:
-			cout << "100";
-			break;
-		case 3:
-			cout << "011";
-			break;
-		case 2:
-			cout << "010";
-			break;
-		case 1:
-			cout << "001";
-			break;
-		case 0:
-			cout << "000";
-			break;
-		default:
-			break;
-		}
+}
+void octNum(int x) {
+	switch (x) {
+	case 7:
+		cout << "111";
+		break;
+	case 6:
+		cout << "110";
+		break;
+	case 5:
+		cout << "101";
+		break;
+	case 4:
+		cout << "100";
+		break;
+	case 3:
+		cout << "011";
+		break;
+	case 2:
+		cout << "010";
+		break;
+	case 1:
+		cout << "001";
+		break;
+	case 0:
+		cout << "000";
+		break;
+	default:
+		break;
 	}
-	cout << '\n';
-	return 0;
 }
