bab541143f33eefb4e4bbbaf9d178b7411b866e0
kkoon9 <rndrnjs2003@naver.com>
2019-07-24 15:16:21 +0900

    Add BOJ10171 고양이
    역슬래시를 출력할 수 있는가를 묻는 문제

---
+++ BOJ10171_Print.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+/*
+\ / \
+)  (')
+(/ )
+\(__) |
+*/
+
+int main() {
+	printf("\\    /\\\n");
+	printf(" )  ( \')\n");
+	printf("(  /  )\n");
+	printf(" \\(__)|\n");
+
+}
