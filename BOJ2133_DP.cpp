8b5acfba204b64fe97c98f8250a1eea6009a101a
kkoon9 <rndrnjs2003@naver.com>
2019-07-24 15:04:02 +0900

    refactoring BOJ2133 타일 채우기
    코드를 함수화하여 main문을 간소화시켰다.

--- BOJ2133_DP.cpp
+++ BOJ2133_DP.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 using namespace std;
-long long memo[31];
+
+const int MAX = 31;
+long long memo[MAX];
+
+void DP(int memo[], int n);
 int main() {
 	int n;
 	cin >> n;
+	DP(memo, n);
+	cout << memo[n] << '\n';
+	return 0;
+}
+
+void DP(int memo[], int n){
 	memo[0] = 1;
 	for (int i = 2; i <= n; i+=2) {
 		memo[i] = memo[i - 2] * 3;
 		for (int j = i - 4; j >= 0; j -= 2)
 			memo[i] += memo[j] * 2;
 	}
-	cout << memo[n] << '\n';
-	return 0;
 }
