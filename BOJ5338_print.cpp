6e20777ebc3597e36eb1c5c393ca23ad403c4164
kkoon9 <rndrnjs2003@naver.com>
2019-08-03 12:06:11 +0900

    Add BOJ5338 마이크로소프트 로고
    백 플래쉬를 써야할 때를 잘 알면 풀 수 있는 문제

---
+++ BOJ5338_print.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	printf("       _.-;;-._\n");
+	printf("\'-..-\'|   ||   |\n");
+	printf("\'-..-\'|_.-\;\;-._|\n");
+	printf("\'-..-\'|   ||   |\n");
+	printf("\'-..-\'|_.-\'\'-._| \n");
+	return 0;
+
+}
