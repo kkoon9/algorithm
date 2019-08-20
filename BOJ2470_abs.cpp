853fbd481ce1bff40a512a8182487edf38715552
kkoon9 <rndrnjs2003@naver.com>
2019-08-20 13:46:37 +0900

    Update BOJ2470 두 용액
    abs를 사용하여 벡터를 정렬 후 비교

--- BOJ2470_abs.cpp
+++ BOJ2470_abs.cpp
@@ -4,6 +4,11 @@
 using namespace std;
 typedef long long ll;
 const long long MAX = 2000000000 + 1;
+
+bool compare(int a, int b) {
+	return abs(a) > abs(b);
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -14,20 +19,16 @@ int main() {
 	vector <long long> v;
 	cin >> N;
 	v.resize(N);
-	for (int i = 0;i < N;i++)
+	for (int i = 0;i < N;i++) {
 		cin >> v[i];
+	}
+	sort(v.begin(), v.end(),compare);
 
-	sort(v.begin(), v.end());
-	for (int i = 0;i < N ;i++) {
-		ll bmp;
-		for (int j = i + 1;j < N;j++) {
-			bmp = v[i] + v[j];
-			cout << result << endl;
-			if (std::abs(result) > std::abs(bmp)) {
-				result = bmp;
-				p.first = v[i];
-				p.second = v[j];
-			}
+	for (int i = 0;i < N - 1;i++) {
+		if (abs(v[i] + v[i + 1]) < result) {
+			result = abs(v[i] + v[i + 1]);
+			p.first = v[i];
+			p.second = v[i + 1];
 		}
 	}
 	if (p.first > p.second)
