5e9afc7ce28795be6d858edc08c73494cf750430
kkoon9 <rndrnjs2003@naver.com>
2019-08-23 15:50:10 +0900

    Add BOJ7570 줄 세우기
    KOI 지역본선 2013

---
+++ BOJ7570_KOI.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+const int MAX = 1000000 + 10;
+int arr[MAX];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int N, r, x= 0;
+	cin >> N;
+	for (int i = 1;i <= N;i++) {
+		cin >> x;
+		arr[x] = arr[x - 1] + 1;
+		if (arr[x] > r) r = arr[x];
+	}
+	cout << N - r << endl;
+	return 0;
+}
